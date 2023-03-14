8375ccf058cf6345283fcb3169e64ce6f47d857b
Itskillerluc <lvgraef@zeelandnet.nl>
2023-03-14 10:11:38 +0100

    Initial commit

---
+++ src/main/java/io/github/itskillerluc/player_combat/PlayerCombat.java
@@ -0,0 +1,21 @@
+package io.github.itskillerluc.player_combat;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(PlayerCombat.MODID)
+public class PlayerCombat
+{
+    public static final String MODID = "player_combat";
+
+    public PlayerCombat()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+}
